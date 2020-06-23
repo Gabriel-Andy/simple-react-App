3448a5365b45b008a28791799eaf98773b1960e9
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2020-06-23 15:56:11 -0400

    we hard coding recipes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
 import React, { useState } from "react";
 import "./App.css";
-import Counter from "./Counter";
-import HookComponent from "./hookComponent";
-export const ThemeContext = React.createContext();
+import RecipeList from "./RecipeList";
 
 function App() {
-  console.log("Render App");
-  const [theme, setTheme] = useState("red");
-  return (
-    <ThemeContext.Provider value={{ backgroundColor: theme }}>
-      Counter
-      <Counter initialCount={0} />
-      CounterHook
-      <HookComponent initialCount={0} />
-      <button
-        onClick={() =>
-          setTheme((prevTheme) => {
-            return prevTheme === "red" ? "blue" : "red";
-          })
-        }
-      >
-        Toggle Theme{" "}
-      </button>
-    </ThemeContext.Provider>
-  );
+  return <RecipeList />;
 }
 
 export default App;
-// setInterval(() => {
-
-// }, 1000);
-
-// prevTheme === "red" ? "blue" : "red";
---
+++ src/RecipeList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Recipe from "./Recipe"
+
+const RecipeList = () => {
+    return (
+        <div>
+            <Recipe/>
+            <Recipe />
+        </div>
+    );
+}
+
+export default RecipeList;
